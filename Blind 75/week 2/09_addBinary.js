140a4a515d8116575045379fd260d7d336738b7f
N <cubingniles@gmail.com>
2022-05-21 19:53:29 +0530

    Blind 75 week 2

---
+++ Blind 75/week 2/09_addBinary.js
@@ -0,0 +1,8 @@
+// method 1 -> using built in methods BigInt()
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+};
+
