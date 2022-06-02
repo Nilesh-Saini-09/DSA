395e5cd70e26d43365312d6f160d1a47c6fae0cb
N <cubingniles@gmail.com>
2022-06-03 01:27:51 +0530

    Sorting Algorithms

--- algorithms/Sorting/bubbleSort.js
+++ algorithms/Sorting/bubbleSort.js
@@ -37,3 +37,19 @@ const bubbleSort2 = arr => {
 
     return arr;
 }
+
+// Recursive bubble sort
+// 1. if array's size is 1 return;
+// 2. Do One Pass of normal Bubble Sort. This pass fixes last element of current subarray.
+// 3. Recur for all the elements in the array except for the last one
+
+// time and space => O(n*n) and O(n)
+
+const recursiveBubbleSort = (arr, n) => {
+    if(n === 1) return;
+    for(let i = 0; i<n-1; i++) {
+        if(arr[i] > arr[i+1]) swap(arr, i, i+1);
+    }
+
+    recursiveBubbleSort(arr, n-1);
+}
