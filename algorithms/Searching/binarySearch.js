da171e7815b8094c1b699aaeee74649cb51dd099
N <cubingniles@gmail.com>
2022-06-19 21:38:54 +0530

    Search Algorithms

--- algorithms/Searching/binarySearch.js
+++ algorithms/Searching/binarySearch.js
@@ -1,35 +1,51 @@
 // recursive
-const bs = (arr, k) => {
+const binarySearch = (arr, k) => {
     let start = 0, end = arr.length - 1, mid;
-    if(start <= end) {
-        mid = Math.floor((start + end) / 2);
-        if(arr[mid] === k) return true;
-        if(arr[mid] > k) return bs(arr.slice(start, mid-1), k);
-        else return bs(arr.slice(mid+1, end+1), k);
-    }
+    mid = Math.floor((start + end) / 2);
+    if(arr.length === 1 && arr[0] === k) return false;
+    if(arr[mid] === k) return mid;
 
-    return false;
+    if(arr[mid] > k) {
+        return binarySearch(arr.slice(0, mid), k);
+    } else {
+        return binarySearch(arr.slice(mid));
+    }
 }
+
+// const testArray = [2, 8, 1, 3, 6, 7, 5, 4];
+// let x = 6
+// search(testArray, x)
+// output => 4
 
 // bs
 let bs2 = function (arr, x, start, end) {
       
     // Base Condition
     if (start > end) return false;
-  
-    // Find the middle index
-    let mid=Math.floor((start + end)/2);
-  
-    // Compare mid with given key x
-    if (arr[mid]===x) return true;
-         
-    // If element at mid is greater than x,
-    // search in the left half of mid
+    let mid = Math.floor((start + end)/2);
+    if (arr[mid] === x) return true;
     if(arr[mid] > x)
         return recursiveFunction(arr, x, start, mid-1);
     else
- 
-        // If element at mid is smaller than x,
-        // search in the right half of mid
         return recursiveFunction(arr, x, mid+1, end);
+}
+
+
+// iterative
+const binarySearch2 = (arr, x) => {   
+    let start = 0;
+    let end = arr.length - 1;
+    let mid;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (arr[mid] === x) return mid;
+  
+        if (arr[mid] < x) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
 }
--- algorithms/Searching/linearSearch.js
+++ algorithms/Searching/linearSearch.js
@@ -1,7 +1,12 @@
 const search = (arr, x) => {
-    for(let i of arr) {
-        if(i === x) return true;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === x) return i;
     }
 
-    return false;
+    return -1;
 }
+
+// const testArray = [2, 8, 1, 3, 6, 7, 5, 4];
+// let x = 6
+// search(testArray, x)
+// output => 4
