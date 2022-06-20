413689fffb49677fec439f8616eaa4bfa4c43f12
N <cubingniles@gmail.com>
2022-06-20 15:10:11 +0530

    Search Algorithms

--- algorithms/Searching/jumpSearch.js
+++ algorithms/Searching/jumpSearch.js
@@ -1,22 +1,19 @@
-const jumpSearch = (arr, x, n) => {
-    // Finding block size to be jumped
-    let step = Math.sqrt(n);
+const jumpSearch = (arr, x) => {
+    let n = arr.length;
+    let block = Math.floor(Math.sqrt(n));
     let prev = 0;
 
-    while (arr[Math.min(step, n)-1] < x) {
-        prev = step;
-        step += Math.sqrt(n);
-        if (prev >= n)
-            return -1;
+    while (arr[Math.min(block, n) - 1] < x) {
+        prev = block;
+        block += Math.sqrt(n);
+        if (prev >= n) return -1;
     }
    
     while (arr[prev] < x) {
         prev++;
-        if (prev == Math.min(step, n))
-            return -1;
+        if (prev == Math.min(block, n)) return -1;
     }
 
-    // If element is found
     if (arr[prev] == x) return prev;
     return -1;
 }
