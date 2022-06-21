ea4861f1e92fee0f2db2f35fd0f785c009021186
N <cubingniles@gmail.com>
2022-06-22 01:45:18 +0530

    Searching Algorithms

---
+++ algorithms/Searching/interpolation.js
@@ -0,0 +1,23 @@
+const interpolationSearch = (arr, lo, hi, x) => {
+    let pos;
+
+    if (lo <= hi && x >= arr[lo] && x <= arr[hi]) {
+
+      pos = lo + Math.floor(((hi - lo) / (arr[hi] - arr[lo])) * (x - arr[lo]));
+        
+      
+        if (arr[pos] == x) return pos;
+        
+     
+        // If x is larger, x is in right sub array
+        if (arr[pos] < x){
+        return interpolationSearch(arr, pos + 1, hi, x);
+        }
+     
+        // If x is smaller, x is in left sub array
+        if (arr[pos] > x){
+        return interpolationSearch(arr, lo, pos - 1, x);
+        }
+    }
+    return -1;
+  }
